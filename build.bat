4da4e23e0c4a36c26ca6dc31fc7fcbef4a48028c
abigsam <abigsam@gmail.com>
2022-05-14 00:11:40 +0300

    Update batch file to use functions; add checking Vivado existing

--- build.bat
+++ build.bat
@@ -5,8 +5,9 @@ ECHO #########################################################
 ECHO # Build Vivado project for MicroPhase Lite
 ECHO # Part: XC7Z010CLG400-1
 ECHO # Author: abigsam@gmail.com
-ECHO # Vivado version: 2020.1
+ECHO # Vivado version: 2021.1
 ECHO #########################################################
+set vivado_path=C:\Xilinx\Vivado\2021.1
 ECHO.
 ECHO Avaliable options:
 ECHO [1] Build project
@@ -38,10 +39,50 @@ rem - run Vivado GUI
 rem - use derictive "exit" at Tcl script
 rem After this, batch file changes run directory to the vivado project ("vivado/" by default) and call for Vivado with GUI
 ECHO Run Tcl build script...
-call C:\Xilinx\Vivado\2020.1\bin\vivado.bat -mode batch -nojournal -source "%~dp0\scripts\build.tcl" -tclargs 0 1
-cd "%~dp0\vivado"
-C:\Xilinx\Vivado\2020.1\bin\unwrapped\win64.o\vvgl.exe C:\Xilinx\Vivado\2020.1\bin\vivado.bat -project "%~dp0\vivado\mp_z7010.xpr"
+CALL :RUN_VIVADO_SCRIPT "%~dp0\scripts\build.tcl" 0 1
+CALL :OPEN_VIVADO_PROJECT "vivado" "mp_z7010"
 goto RUN_CLEANUP
 
 :END
 rem pause
+
+
+:RUN_VIVADO_SCRIPT
+    set vivado_bat_path=%vivado_path%\bin\vivado.bat
+    if not exist %vivado_bat_path% (
+        echo ERROR! Path %vivado_bat_path% did not found
+        pause
+        exit
+    )
+    if not exist %~1 (
+        echo ERROR! Script file %~1 did not found
+        pause
+        exit
+    )
+    call %vivado_bat_path% -mode batch -nojournal -notrace -source %~1 -tclargs %~2 %~3 %~4 %~5
+EXIT /B 0
+
+
+:OPEN_VIVADO_PROJECT
+    if "%~2"=="" (
+        set xpr_name=%~1.xpr
+    ) else (
+        set xpr_name=%~2.xpr
+    )
+    set prj_path=%~dp0\%~1\%xpr_name%
+    set vivado_bat_path=%vivado_path%\bin\vivado.bat
+    set vivado_vvgl_path=%vivado_path%\bin\unwrapped\win64.o\vvgl.exe
+    if not exist %vivado_bat_path% (
+        echo ERROR! Path %vivado_bat_path% did not found
+        pause
+        exit
+    )
+    if not exist %prj_path% (
+        echo ERROR! Vivado project file %prj_path% did not found
+        pause
+        exit
+    )
+    pushd "%~dp0\vivado"
+    call %vivado_vvgl_path% %vivado_bat_path% -project %prj_path%
+    popd
+EXIT /B 0

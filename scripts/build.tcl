f5c521d00528e7c19263f1f86f8c78fa965d48e5
abigsam <abigsam@gmail.com>
2022-05-14 22:52:00 +0300

    Update build BD scripts; ZYNQ7 now configure from preset

--- scripts/build.tcl
+++ scripts/build.tcl
@@ -1,21 +1,35 @@
+#@Note Read Tcl parameters
+#
+#@param num         Parameter number
+#@default_value     Default value if there is no parameter with requested number
+#@retval            Parameter value
+proc read_param {num default_value} {
+    if {[llength $::argv] > $num} {
+        return [lindex $::argv $num]
+    } else {
+        return ${default_value}
+    }
+}
+
+#Get script path
+set script_path     [file dirname [file normalize [info script]]]
 
 #Configure project
 set prj_name        "mp_z7010"
-set script_path     [file dirname [file normalize [info script]]]
 set part_name       "xc7z010clg400-1"
 set prj_path        [file normalize ${script_path}/../vivado]
 set board_path      [file normalize ${script_path}/../board]
-set bd_name         "design_1"
-set ps_name         "proc_sys"
 
 #Aux. processes
 source ${script_path}/aux_proc.tcl
 source ${script_path}/create_bd.tcl
+#Source script with Zynq presets
+source ${board_path}/microphase_lite_zynq.tcl
 
 #Gets arguments
 set create_rtl_design 0
 if { [llength $argv] > 0 } {
-    if { [lindex $argv 0] == "create_rtl"} {
+    if { [lindex $argv 0] == "create_hdl"} {
         set create_rtl_design 1
     }
 }
@@ -23,18 +37,19 @@ if { [llength $argv] > 0 } {
 #Create project
 create_project ${prj_name} ${prj_path} -part ${part_name} -force
 
+#Add constraints
+add_files -fileset constrs_1 -norecurse [findFiles ${board_path} "*.xdc"]
+
+#Create block design or use pure HDL (FPGA only)
 if {${create_rtl_design} == 0} {
-    create_bd ${bd_name} ${ps_name} ${board_path}
+    create_bd ${board_path}
 } else {
-    #RTL design
-    add_files -fileset constrs_1 -norecurse [list ${board_path}/pl_pins.xdc ${board_path}/config.xdc]
+    #HDL design
 
 }
 
-
 #Create xcleanup.bat file
 create_prj_cleanup ${prj_path} ${prj_name}
-
 
 ##Run build
 # launch_runs impl_1 -to_step write_bitstream -jobs 16
--- scripts/create_bd.tcl
+++ scripts/create_bd.tcl
@@ -1,71 +1,31 @@
-proc create_bd {bd_name ps_name constr_path} {
-    #Add ZYNQ PS
+proc create_bd {constr_path} {
+    set bd_name "design_1"
+    
+    #Add ZYNQ PS **********************************************************************************
     create_bd_design ${bd_name}
-    create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 ${ps_name}
-
-    #Configure DDR
-    set_property -dict [list \
-        CONFIG.PCW_UIPARAM_DDR_BUS_WIDTH {16 Bit} \
-        CONFIG.PCW_UIPARAM_DDR_PARTNO {MT41K256M16 RE-125}] \
-    [get_bd_cells ${ps_name}]
-
-    #Disable GP0
-    set_property -dict [list CONFIG.PCW_USE_M_AXI_GP0 {0}] [get_bd_cells ${ps_name}]
-
-    #Configure periphery
-    #QSPI
-    set_property -dict [list \
-        CONFIG.PCW_QSPI_PERIPHERAL_ENABLE {1} \
-        CONFIG.PCW_QSPI_QSPI_IO {MIO 1 .. 6}\
-    ] [get_bd_cells ${ps_name}]
-
-    #UART
-    set_property -dict [list \
-        CONFIG.PCW_UART0_BAUD_RATE {115200} \
-        CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1} \
-        CONFIG.PCW_UART0_PERIPHERAL_ENABLE {1} \
-        CONFIG.PCW_UART0_UART0_IO {MIO 14 .. 15} \
-        CONFIG.PCW_UART0_GRP_FULL_ENABLE {0}\
-    ] [get_bd_cells ${ps_name}]
-
-    #SD
-    set_property -dict [list \
-        CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1} \
-        CONFIG.PCW_SD0_PERIPHERAL_ENABLE {1} \
-        CONFIG.PCW_SD0_SD0_IO {MIO 40 .. 45}\
-    ] [get_bd_cells ${ps_name}]
-
-    #USB
-    set_property -dict [list \
-        CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1} \
-        CONFIG.PCW_USB0_PERIPHERAL_ENABLE {1} \
-        CONFIG.PCW_USB0_USB0_IO {MIO 28 .. 39} \
-        CONFIG.PCW_USB0_RESET_ENABLE {1} \
-        CONFIG.PCW_USB0_RESET_IO {MIO 46} \
-        CONFIG.PCW_I2C_RESET_ENABLE {0} \
-        CONFIG.PCW_GPIO_MIO_GPIO_ENABLE {1}\
-    ] [get_bd_cells ${ps_name}]
-
+    set ps_ip [create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 "proc_sys"]
+    #Configure ZYNQ
+    set_property -dict [apply_preset ${ps_ip}] [get_bd_cells ${ps_ip}]; #From presets
     #External ZYNQ IOs
-    apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {make_external "FIXED_IO, DDR" Master "Disable" Slave "Disable" }  [get_bd_cells ${ps_name}]
+    apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {\
+        make_external "FIXED_IO, DDR" Master "Disable" Slave "Disable" } \
+    [get_bd_cells ${ps_ip}]
+
+    #User code ************************************************************************************
+
+
+    #Connect PL LEDs to '1' to prevent false on
+    create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 pl_led_dis
+    connect_bd_net [get_bd_ports [create_bd_port -dir O PL_LED1]] [get_bd_pins pl_led_dis/dout]
+    connect_bd_net [get_bd_ports [create_bd_port -dir O PL_LED2]] [get_bd_pins pl_led_dis/dout]
+    save_bd_design
+
+    #Validate *************************************************************************************
     validate_bd_design
     save_bd_design
 
-    #Connect PL LEDs to '1'
-    add_files -fileset constrs_1 -norecurse ${constr_path}/pl_pins.xdc
-    set_property used_in_synthesis false [get_files  ${constr_path}/pl_pins.xdc]
-    create_bd_port -dir O PL_LED1
-    create_bd_port -dir O PL_LED2
-    create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 plleddis1
-    create_bd_cell -type ip -vlnv xilinx.com:ip:xlconstant:1.1 plleddis2
-    connect_bd_net [get_bd_ports PL_LED1] [get_bd_pins plleddis1/dout]
-    connect_bd_net [get_bd_ports PL_LED2] [get_bd_pins plleddis2/dout]
-
-    #Validate
-    validate_bd_design
-    save_bd_design
-
-    #Make wrapper
+    #Make wrapper *********************************************************************************
     set wrapper_path [make_wrapper -fileset sources_1 -files [ get_files -norecurse ${bd_name}.bd] -top]
     add_files -norecurse -fileset sources_1 ${wrapper_path}
+    return [file tail ${wrapper_path}]; #Return wrapper file name
 }
